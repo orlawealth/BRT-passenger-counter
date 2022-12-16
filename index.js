5e85549bdf851ac095545d82f123e17e69d370f9
orlawealth <52291327+orlawealth@users.noreply.github.com>
2022-12-16 05:02:20 +0100

    First commit
    Ready to deploy

---
+++ index.js
@@ -0,0 +1,19 @@
+//camelCase
+let saveEl = document.getElementById("save-el") 
+let countEl = document.getElementById("count-el") //pass in arguments
+let count = 0
+
+function increment(){
+    count += 1
+    countEl.innerText = count
+}
+
+
+
+function save(){
+    let countStr = " " + count + " -"
+    saveEl.innerText += countStr
+    countEl.innerText = 0
+    count = 0
+}
+
